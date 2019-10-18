9dde25f620b46b713d9acf899aded3808ebc3e37
stefanprodan <stefan.prodan@gmail.com>
2019-10-18 23:26:55 +0300

    Format hrval

--- .circleci/hrval.sh
+++ .circleci/hrval.sh
@@ -4,33 +4,16 @@ set -o errexit
 
 HR=$1
 IGNORE_VALUES=$2
+
 usage() {
   echo "$(basename $0) </path/to/helmrelease>"
 }
 
-if test ! -f "${HR}"
-then
-  echo "Error! \"${HR}\" file not found!"
-  echo
+if test ! -f "${HR}"; then
+  echo "\"${HR}\" file not found!"
   usage
-  exit 1;
+  exit 1
 fi
-
-TMPDIR=$(mktemp -d)
-
-function clone {
-  ORIGIN=$(git rev-parse --show-toplevel)
-  REPO=$(yq r ${1} spec.chart.git)
-  REF=$(yq r ${1} spec.chart.ref)
-  CHART_PATH=$(yq r ${HR} spec.chart.path)
-  cd ${2}
-  git init -q
-  git remote add origin ${REPO}
-  git fetch -q origin
-  git checkout -q ${REF}
-  cd ${ORIGIN}
-  echo ${2}/${CHART_PATH}
-}
 
 function download {
   CHART_REPO=$(yq r ${1} spec.chart.repository)
@@ -42,33 +25,50 @@ function download {
   echo ${CHART_TAR}
 }
 
-CHART_PATH=$(yq r ${HR} spec.chart.path)
+function clone {
+  ORIGIN=$(git rev-parse --show-toplevel)
+  GIT_REPO=$(yq r ${1} spec.chart.git)
+  GIT_REF=$(yq r ${1} spec.chart.ref)
+  CHART_PATH=$(yq r ${1} spec.chart.path)
+  cd ${2}
+  git init -q
+  git remote add origin ${GIT_REPO}
+  git fetch -q origin
+  git checkout -q ${GIT_REF}
+  cd ${ORIGIN}
+  echo ${2}/${CHART_PATH}
+}
 
-if [ "${CHART_PATH}" == "null" ]; then
-  echo "Downloading to ${TMPDIR}"
-  CHART_TAR=$(download ${HR} ${TMPDIR}| tail -n1)
-else
-  echo "Cloning to ${TMPDIR}"
-  CHART_TAR=$(clone ${HR} ${TMPDIR}| tail -n1)
-fi
+function validate {
+  TMPDIR=$(mktemp -d)
+  CHART_PATH=$(yq r ${HR} spec.chart.path)
 
-HR_NAME=$(yq r ${HR} metadata.name)
-HR_NAMESPACE=$(yq r ${HR} metadata.namespace)
+  if [ "${CHART_PATH}" == "null" ]; then
+    echo "Downloading to ${TMPDIR}"
+    CHART_TAR=$(download ${HR} ${TMPDIR}| tail -n1)
+  else
+    echo "Cloning to ${TMPDIR}"
+    CHART_TAR=$(clone ${HR} ${TMPDIR}| tail -n1)
+  fi
 
-echo "Extracting values to ${TMPDIR}/${HR_NAME}.values.yaml"
-if [ ${IGNORE_VALUES} ]; then
-  echo "" > ${TMPDIR}/${HR_NAME}.values.yaml
-else
-  yq r ${HR} spec.values > ${TMPDIR}/${HR_NAME}.values.yaml
-fi
+  HR_NAME=$(yq r ${HR} metadata.name)
+  HR_NAMESPACE=$(yq r ${HR} metadata.namespace)
 
-echo "Writing Helm release to ${TMPDIR}/${HR_NAME}.release.yaml"
-helm template ${CHART_TAR} \
---name ${HR_NAME} \
---namespace ${HR_NAMESPACE} \
--f ${TMPDIR}/${HR_NAME}.values.yaml > ${TMPDIR}/${HR_NAME}.release.yaml
+  echo "Extracting values to ${TMPDIR}/${HR_NAME}.values.yaml"
+  if [ ${IGNORE_VALUES} ]; then
+    echo "" > ${TMPDIR}/${HR_NAME}.values.yaml
+  else
+    yq r ${HR} spec.values > ${TMPDIR}/${HR_NAME}.values.yaml
+  fi
 
-echo "Validating Helm release ${HR_NAME}.${HR_NAMESPACE}"
-kubeval --strict --ignore-missing-schemas ${TMPDIR}/${HR_NAME}.release.yaml
+  echo "Writing Helm release to ${TMPDIR}/${HR_NAME}.release.yaml"
+  helm template ${CHART_TAR} \
+  --name ${HR_NAME} \
+  --namespace ${HR_NAMESPACE} \
+  -f ${TMPDIR}/${HR_NAME}.values.yaml > ${TMPDIR}/${HR_NAME}.release.yaml
 
-rm -rf ${TMPDIR}
+  echo "Validating Helm release ${HR_NAME}.${HR_NAMESPACE}"
+  kubeval --strict --ignore-missing-schemas ${TMPDIR}/${HR_NAME}.release.yaml
+}
+
+validate
